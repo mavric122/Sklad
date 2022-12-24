9ef827b117c7e5bf1eb8c71626fb415402e85933
mavric <mavric122@gmail.com>
2022-12-24 14:05:22 +0300

    Исправления в связи с переходом с функций на классы

--- sklad/tovar/urls.py
+++ sklad/tovar/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', sklad, name='mavric'),
-    path('category/', tovar_list_category, name='spisok_category'),
-    path('category/<int:category_id>/', tovar_get_category, name='category_id'),
-    path('category/list_tovar/', all_tovars, name='all_tovars'),
-    path('tovar/id/<int:tovar_id>', view_tovar, name='view_tovar'),
-    path('category/add_category', add_category, name='add_category'),
-    path('tovar/add_tovar', add_tovar, name='add_tovar'),
-    path('tovar/edit_tovar/id/<int:tovar_id>', edit_tovar, name='edit_tovar'),
+    path('', Sklad.as_view(), name='mavric'),
+    path('category/', ListCategory.as_view(), name='spisok_category'),
+    path('category/<int:category_id>/', GetCategory.as_view(), name='category_id'),
+    path('category/list_tovar/', AllTovars.as_view(), name='all_tovars'),
+    path('tovar/id/<int:tovar_id>', ViewTovar.as_view(), name='view_tovar'),
+    path('category/add_category', AddCategory.as_view(), name='add_category'),
+    path('tovar/add_tovar', AddTovar.as_view(), name='add_tovar'),
+    path('tovar/edit_tovar/id/<int:pk>', UpdateTovar.as_view(), name='edit_tovar'),
+    path('__debug__/', include('debug_toolbar.urls')),
     # path('increase_counter/', increasecounter, name='increase-counter')
 
 ]
