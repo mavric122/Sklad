3847ef320b9ca44eabffbe3da4290ab8aa2785e7
mavric <mavric122@gmail.com>
2022-12-25 16:40:07 +0300

    Добавлен класс регистрации пользователя

--- sklad/tovar/forms.py
+++ sklad/tovar/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from .models import Category, Tovar
 
 
@@ -51,3 +52,25 @@ class CategoryForm(forms.ModelForm):
         widgets = {  # Внимание!!! В виджетах используются не аргумент для поля, а аргумент для виджета.
             'title': forms.TextInput(attrs={"class": "form-control"})
         }
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(max_length='50', label='Имя пользователя',
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(max_length='50', label='Пароль',
+                             widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(max_length='50', label='Пароль',
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(max_length='50', label='Пароль 2',
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(max_length='50', label='Имя пользователя',
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(max_length='50', label='Пароль',
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
