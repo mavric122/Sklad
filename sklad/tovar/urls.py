27906553461635014b85c0e8d38bf19d7f859281
mavric <mavric122@gmail.com>
2022-12-25 16:41:34 +0300

    Добавлен 1)Login 2)Logout 3)Register 4)Search

--- sklad/tovar/urls.py
+++ sklad/tovar/urls.py
@@ -3,6 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', Sklad.as_view(), name='mavric'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+    path('search/', Search.as_view(), name='search'),
     path('category/', ListCategory.as_view(), name='spisok_category'),
     path('category/<int:category_id>/', GetCategory.as_view(), name='category_id'),
     path('category/list_tovar/', AllTovars.as_view(), name='all_tovars'),
@@ -12,5 +16,5 @@ urlpatterns = [
     path('tovar/edit_tovar/id/<int:pk>', UpdateTovar.as_view(), name='edit_tovar'),
     path('__debug__/', include('debug_toolbar.urls')),
     # path('increase_counter/', increasecounter, name='increase-counter')
-
 ]
+# <str:search>/
